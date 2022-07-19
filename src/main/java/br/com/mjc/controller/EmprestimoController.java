76fbe3e3712e16f3330e241b9aed43cff9b338ee
Marcio <marcio.nextage@gmail.com>
2022-07-19 18:00:51 -0300

    :package: feature: envia os valores da view para o controller

--- src/main/java/br/com/mjc/controller/EmprestimoController.java
+++ src/main/java/br/com/mjc/controller/EmprestimoController.java
@@ -1,5 +1,7 @@
 package br.com.mjc.controller;
 import br.com.mjc.service.EmprestimoService;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class EmprestimoController {
     public static String emprestar(String nChave, String ra, String senha){
